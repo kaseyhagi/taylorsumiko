408727cdda264426a31b257bb96f54ddec108d09
kaseyhagi <khagi@hawaii.edu>
2018-05-10 15:58:28 -1000

    Update tay-info.js

--- tay-info.js
+++ tay-info.js
@@ -1,5 +1,6 @@
 var info = {
     intro: "u know my swag not my story",
-    skills: []
+    skills: ["Screening calls","Listens to the same 5 songs", "On again/off again ho","Impulse shopping","Calling out of work",
+            "Early to  everything"]
     
 };
