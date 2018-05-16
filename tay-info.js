8e55eb1d72eca2779769ae69ed31ab0062a6de2e
kaseyhagi <khagi@hawaii.edu>
2018-05-16 12:19:57 -1000

    carousel

--- tay-info.js
+++ tay-info.js
@@ -1,6 +1,8 @@
 var info = {
     intro: "u know my swag not my story",
     skills: ["Screening calls","Listens to the same 5 songs", "On again/off again ho","Impulse shopping","Calling out of work",
-            "Early to  everything"]
+            "Early to  everything"],
+    images: ["0009","1566","6708","6709","6710","6711"
+                      ,"6712","6713","6714","6715","6716","6717","67081"]
     
 };
